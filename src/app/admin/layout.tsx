b1997541a7ea6b67c136d4e54ef75fcd3683c797
fahadkabali <kabalifahad@gmail.com>
2024-11-24 00:30:27 +0300

    added footer and header components for the dashoard

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,12 +1,32 @@
+import { ADMIN } from "@/constants/constants";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const supabase = createClient();
+  const { data: authData } = await (await supabase).auth.getUser();
+  if(authData?.user) {
+      const { data, error } = await (await supabase)
+      .from('users')
+      .select('*')
+      .eq('id', authData.user.id)
+      .single();
+      if(error || !data) {
+          console.log('Error fetching user data',error);
+          return
+      }
+      if(data.type === ADMIN) {
+          return redirect('/');
+      }
+  }
   return (      
-    <div>
+    <>
         {children}
-    </div> 
+    </> 
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased` }
       >
         <ThemeProvider
             attribute="class"
@@ -37,7 +37,7 @@ export default async function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-        <main>
+        <main >
           {children}
         </main>
         <Toaster richColors />
